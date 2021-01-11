d492b4b684074636f45574a3693a360c0b05358c
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-11 16:13:04 -0500

    fixed something

--- src/conversion.js
+++ src/conversion.js
@@ -110,21 +110,21 @@ exports.hslToRgb = function (h, s, l) {
 }
 
 exports.rgbToHsv = function (r, g, b) {
-  r /= 255, g /= 255, b /= 255;
-
-  let max = Math.max(r, g, b), min = Math.min(r, g, b),
-     h, s, v = max;
-
-  let d = max - min;
+	let r1 /= 255;
+	let g1 /= 255;
+	let b1 /= 255;
+	let max = Math.max(r1, g1, b1), min = Math.min(r1, g1, b1),
+	  h, s, v = max,
+	  d = max - min;
   s = max == 0 ? 0 : d / max;
 
   if (max == min)
     h = 0; // achromatic
   else {
     switch (max) {
-      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-      case g: h = (b - r) / d + 2; break;
-      case b: h = (r - g) / d + 4; break;
+      case r: h = (g1 - b1) / d + (g1 < b1 ? 6 : 0); break;
+      case g: h = (b1 - r1) / d + 2; break;
+      case b: h = (r1 - g1) / d + 4; break;
     }
 
     h /= 6;
